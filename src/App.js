b820c08ddf492dc183b51a82ed52f76d5e61e675
manish <manish@cryptorai.com>
2019-05-20 16:02:13 -0500

    Hello change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye there and hi there and!
+          Hello There!
         </p>
         <a
           className="App-link"
